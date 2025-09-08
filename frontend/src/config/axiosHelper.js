c0897192871af86509eced64e530f8c06b0e03d6
sudhirskp <gnitwallah@gmail.com>
2025-09-08 11:52:08 +0530

    icon ,url update

--- frontend/src/config/axiosHelper.js
+++ frontend/src/config/axiosHelper.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+// export const baseURL = "http://localhost:8080";
 export const baseURL = "https://chatapp-production-cac5.up.railway.app";
 export const HttpClient = axios.create({
     baseURL: baseURL,
