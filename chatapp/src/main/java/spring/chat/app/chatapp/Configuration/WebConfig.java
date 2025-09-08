8dc6e129e62e772ba4d12a9b3cad77fe8f38431d
sudhirskp <gnitwallah@gmail.com>
2025-09-08 12:09:30 +0530

    final finishing

--- chatapp/src/main/java/spring/chat/app/chatapp/Configuration/WebConfig.java
+++ chatapp/src/main/java/spring/chat/app/chatapp/Configuration/WebConfig.java
@@ -5,20 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOriginPatterns("https://courageous-cheesecake-916884.netlify.app/")  // Use allowedOriginPatterns instead of allowedOrigins
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);  // Set to true if you need to send credentials
-//    }
-//}
 
- //WebConfig.java
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
