5944f20f7218cd3610366c43492e6a90dc9d3404
sudhirskp <gnitwallah@gmail.com>
2025-10-04 21:51:33 +0530

    backend render

--- frontend/src/config/axiosHelper.js
+++ frontend/src/config/axiosHelper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // export const baseURL = "http://localhost:8080";
-export const baseURL = "https://chatapp-production-cac5.up.railway.app";
+export const baseURL = "https://chatback-latest-4xc1.onrender.com";
 export const HttpClient = axios.create({
     baseURL: baseURL,
     headers: {
