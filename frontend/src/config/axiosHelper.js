62961a5371c04e770ca7715275e31ca9410f5e93
sudhirskp <gnitwallah@gmail.com>
2025-10-19 13:11:20 +0530

    update

--- frontend/src/config/axiosHelper.js
+++ frontend/src/config/axiosHelper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // export const baseURL = "http://localhost:8080";
-export const baseURL = "https://chatback-v1-1.onrender.com/";
+export const baseURL = "https://chatback-v1-1.onrender.com";
 export const HttpClient = axios.create({
     baseURL: baseURL,
     headers: {
