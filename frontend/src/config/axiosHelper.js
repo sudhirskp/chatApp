0a70358dfa84e3510948219dddfe4d6fa8e6b9e4
sudhirskp <gnitwallah@gmail.com>
2025-11-15 22:50:00 +0530

    update baseURL to use secure HTTPS link

--- frontend/src/config/axiosHelper.js
+++ frontend/src/config/axiosHelper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // export const baseURL = "http://localhost:8080";
-export const baseURL = "http://139.59.88.227:8080";
+export const baseURL = "https://skproom.me";
 export const HttpClient = axios.create({
     baseURL: baseURL,
     headers: {
